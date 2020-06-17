935d7f1aedb89afa1746b22f52a9105499f7857d
Airton1160 <liriungo@outlook.com.br>
2020-06-17 13:43:23 -0300

    front pronto sem reestruturação

--- src/server.js
+++ src/server.js
@@ -3,14 +3,23 @@ const server = express()
 //configurando uma pasta publica
 server.use(express.static("public"))// pasta com .css's
 
+//ultilizando template engine, configurando o nunjucks
+const nunjucks = require("nunjucks")//aqui eh tipo instanciar
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
 
 // configurar caminhos da minha aplicação
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")// AQUI ENVIANDO A PAGINA INDEX, BUSCANDO NO PC
+    return res.render("index.html", {airton: "aqui mano airton vai dar certo o emprego"})// AQUI ENVIANDO A PAGINA INDEX, BUSCANDO NO PC
 })
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 } )
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
 // página inicial
 //ligando o server
 server.listen(3000)
